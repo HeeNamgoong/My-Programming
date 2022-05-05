0aad93cb348ae7a54c404dba1ba11f57001c4064
HeeNamgoong <104904309+HeeNamgoong@users.noreply.github.com>
2022-05-05 15:57:15 +0900

    Delete jaccard_20223068.py

--- jaccard_20223068.py
+++
@@ -1,9 +0,0 @@
-allSet = [{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 3, 5, 7, 12, 15}, {3, 12, 15, 18, 20}, {12, 13, 14, 15, 16, 17, 18, 19}]
-
-cnt = 3
-for i in range(len(allSet)):
-    for j in range(cnt):
-        J = len(allSet[i] & allSet[i+j+1]) / len(allSet[i] | allSet[i+j+1])
-        print(J)
-    print('-'*60)
-    cnt -= 1
